38f6eee62d8c11085f11a6b96087aad0ba8c3a30
Andreas Molzer <andreas.molzer@gmx.de>
2021-03-24 04:20:35 +0100

    Fix synchronization issue on join
    Here the client did not wait long enough. This meant that the server was
    still resetting the queues which could drop requests by a client by
    storing 0 in the write_head, which was assumed to be controlled by the
    newly joining client.
    
    The issue was that the client accepted _any_ waking from the futex as
    sufficient signal that the server had looked at its request. But indeed,
    a second client that joins and blocks might also wake it instead. We
    now utilize the bitset feature which requires us to only support
    `i32::MAX` clients at most. The highest bit is used to signal the empty
    slot state. This allows us to use wait_bitset with which we
    differentiate between bad wakes and successful wakes.

--- src/linux.rs
+++ src/linux.rs
@@ -1,6 +1,6 @@
 use super::{ConnectError, ShmClient, ShmController};
 
-use core::sync::atomic::Ordering;
+use core::sync::atomic::{fence, Ordering};
 use linux_futex::{AsFutex, Futex, Shared, WaitError};
 
 // Enable futex based join.
@@ -13,23 +13,36 @@ pub fn own_pid() -> u64 {
 }
 
 pub fn futex(client: &ShmClient, this: u32) -> Result<u32, ConnectError> {
+    assert!(this <= i32::MAX as u32, "Can't signal more than 0x7fff_ffff clients with futex");
     let open = client.open();
     let open = &open.futex;
 
-    while let Err(err) = open.compare_exchange(-1, this as i32, Ordering::AcqRel, Ordering::Relaxed) {
+    // Acquire the slot within the open queue
+    while let Err(err) = open.compare_exchange(-1, this as i32, Ordering::AcqRel, Ordering::Acquire) {
         let futex: &Futex<Shared> = open.as_futex();
-        futex.wake(1);
         // Shortly wait for this to clear. Never mind if it did already.
-        let _ = futex.wait(err);
+        //
+        // Point (a), wait on entry to critical section.
+        let _ = futex.wait_bitset(err, 0x1 << 31);
     }
 
     loop {
-        match (open.as_futex() as &Futex<Shared>).wait(this as i32) {
+        // Wait for a reset with highest bit set, i.e. write of `-1` to the slot, i.e. reacquired
+        // by the server itself after it added us.
+        match (open.as_futex() as &Futex<Shared>).wait_bitset(this as i32, 0x1 << 31) {
+            Ok(()) => {},
             // If someone else updated before we woke, we might have a wrong value but succeeded.
-            Ok(()) | Err(WaitError::WrongValue) => break,
+            Err(WaitError::WrongValue) => break,
             Err(WaitError::Interrupted) => {},
         }
     }
+
+    // We've left the critical section. There may be more clients waiting at (a)
+    (open.as_futex() as &Futex<Shared>).wake(i32::MAX);
+
+    // The server writes the mutex with release, ensuring all state management completed. We will
+    // need to acquire it to complete the transfer of the queue state to us.
+    fence(Ordering::Acquire);
 
     // Queues are ID mapped for clients..
     Ok(this)
@@ -39,6 +52,8 @@ pub fn join_futex(server: &ShmController, transaction: impl FnOnce(u32)) {
     let join_futex = &server.open().futex;
     let request = join_futex.load(Ordering::Acquire) as u32;
     if request < server.monitor().max_members.load(Ordering::Relaxed) {
+        assert!(request <= i32::MAX as u32, "Can't signal more than 0x7fff_ffff clients with futex");
+
         // We must reset the queue. Assumes there is no current user here. The old secondary
         // was apparently gone and we would be the primary.
         transaction(request);
@@ -49,6 +64,6 @@ pub fn join_futex(server: &ShmController, transaction: impl FnOnce(u32)) {
     } else if request != u32::MAX {
         // No idea what that was but reset so that other clients can signal.
         join_futex.store(-1, Ordering::Release);
-        (join_futex.as_futex() as &Futex<Shared>).wake(1);
+        (join_futex.as_futex() as &Futex<Shared>).wake(i32::MAX);
     }
 }
